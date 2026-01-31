ee58ee0b8dd07f6911f51e357de0ecca0b8693d7
RickRuizJ <rruizart@yahoo.com>
2026-01-31 14:59:05 -0600

    Fix backend models, sequelize config, and app setup

--- src/models/Submission.js
+++ src/models/Submission.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const sequelize = require('../config/database');
 
 const Submission = sequelize.define('Submission', {
   id: {
